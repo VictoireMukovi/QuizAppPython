5ab38702baff4d240fd371a197a9347f86589d1c
VictoireMukovi <victoirepalukumukovi@gmail.com>
2024-03-26 21:24:48 +0100

    Initialisation du projet

---
+++ quizApp.py
@@ -0,0 +1,6 @@
+
+score =0
+def poser_question(question,r1,r2,r3,r4,bonne_reponse):
+    global score
+    print("Question : \n"+question+"\n choissisez la bonne réponse \n"+ "a. ",r1,"\n b. ",r2," \n c. ",r3," \ d. ",r4)
+    
