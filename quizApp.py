afc8e8a0f360d52b3d6c6571f35f14d3213a53c8
VictoireMukovi <victoirepalukumukovi@gmail.com>
2024-03-26 21:58:36 +0100

    Premiere test réussi

--- quizApp.py
+++ quizApp.py
@@ -2,5 +2,14 @@
 score =0
 def poser_question(question,r1,r2,r3,r4,bonne_reponse):
     global score
-    print("Question : \n"+question+"\n choissisez la bonne réponse \n"+ "a. ",r1,"\n b. ",r2," \n c. ",r3," \ d. ",r4)
-    
+    print("Question : \n"+question+"\n choissisez la lettre qui corresponde à la bonne réponse \n"+ " a. ",r1,"\n b. ",r2," \n c. ",r3," \n d. ",r4)
+    reponse= input("Votre réponse : ") 
+    if reponse==bonne_reponse:
+        score+=10
+        print("Bonne réponse !")
+    else:
+        print("Mauvaise réponse !")
+poser_question("Quel est la capital de l'Espagne ","Paris","Barcelone","Madrid","Seville","c")
+poser_question("Quel est la langue officiel de la chine ","Mandarin","Anglais","Français","Chinois","a")
+poser_question("Parmis ces gens l'un n'est pas parmis les 12 disciples  de Jésus","Mathieu","Paul","Simon Pierre","Phillipe","b")
+poser_question("En quel savan nous dévons le mérité d'avoir découvert la loi d'inertie ","Jean Paul sartre","Archimede","Gandhi","Socrate","b")
